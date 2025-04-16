28220766ee86489d3d1c94a86a04cf2dda97647d
daniel-agblevor <mcdanagb@gmail.com>
2025-04-16 12:18:52 +0000

    Task 8: Set up Login Serializer [Create MyTokenObtainPairSerializer Class to generate and refresh token].

---
+++ core/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.name
+        # ...add other custom data you want in the token
+
+        return token
