77e2d60900f708f2ee8fecbf33a6add821cec767
Desmond Opoku Anane <doanane@st.ug.edu.gh>
2026-07-18 09:55:10 +0000

    Delete backend directory

--- backend/userauths/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from userauths import views
-
-urlpatterns = [
-    path("token/", views.MyTokenObtainPairView.as_view()),
-    path("register/", views.RegisterView.as_view()),
-    path('password-reset/', views.PasswordResetEmailView.as_view(), name='password-reset'),
-    path('reset-password/<str:encoded_pk>/<str:token>/', views.PasswordResetConfirmView.as_view(), name='reset-password'),
-]
